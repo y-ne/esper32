898536b7a7c0fb74a4240fb8a7d97ff3ed410dbf
Yy <akumakumu@gmail.com>
2024-04-23 14:21:46 +0700

    base 128x64 oled display code

---
+++ sketch/ssd1306-128x64.cpp
@@ -0,0 +1,31 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_I2C_ADDR 0x3C // or 0x3D
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RST_PIN -1
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST_PIN);
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_I2C_ADDR);
+
+  hello();
+}
+
+void loop() {
+  
+}
+
+void hello() {
+    display.clearDisplay();
+
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+
+    display.println("Youne");
+    display.display();
+}
