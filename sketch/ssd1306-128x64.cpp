fdaa8bf8bf00cfde39aca023f0cea2edf4119afe
Yy <akumakumu@gmail.com>
2024-04-23 14:43:42 +0700

    adding custom SDA SCL pin

--- sketch/ssd1306-128x64.cpp
+++ sketch/ssd1306-128x64.cpp
@@ -2,6 +2,10 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+// Assign I2C Pin, comment if using default pin
+// #define I2C_SDA 33
+// #define I2C_SCL 32
+
 #define SCREEN_I2C_ADDR 0x3C // or 0x3D
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
